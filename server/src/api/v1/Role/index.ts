aa874d67d59940d480e01754327a08c59ae0e130
clintjeff2 <clintjeff2@gmail.com>
2025-07-30 01:11:59 +0100

    feat: add role module exports and configuration
    - Export all role management components
    - Configure module dependencies and services
    - Provide centralized role system access

---
+++ server/src/api/v1/Role/index.ts
@@ -0,0 +1,6 @@
+export { default as RoleController } from "./role.controller";
+export { default as RoleManagementController } from "./roleManagement.controller";
+export { default as RoleService } from "./role.service.simple";
+export { UserRole, IRoleAssignmentRequest, IRoleQueryFilters } from "./role.types";
+export { default as roleRoutes } from "./role.routes";
+export { default as roleManagementRoutes } from "./roleManagement.routes";
