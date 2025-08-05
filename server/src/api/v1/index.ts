2036bd55144e904ef8bdc9cae9393c74dc4a43f8
Isaac Gellu <113101848+gelluisaac@users.noreply.github.com>
2025-08-05 12:59:26 +0100

    Implement Admin Management Module (#270)
    * Implement Admin Management Module
    
    * Update Admin module to use tsyringe

--- server/src/api/v1/index.ts
+++ server/src/api/v1/index.ts
@@ -4,7 +4,6 @@ import userRouter from "./User/user.routes";
 import adminRouter from "./Admin/admin.routes";
 
 const v1: Router = Router();
-
 v1.use("/auth", authRouter);
 v1.use("/user", userRouter);
 v1.use("/admin", adminRouter);
